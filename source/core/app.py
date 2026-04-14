74593e6f7c33f1d3156cc01c0965173a31d35b7e
thanhan0404 <thanhan0404@gmail.com>
2026-04-14 08:13:46 +0000

    fix port

--- source/core/app.py
+++ source/core/app.py
@@ -33,4 +33,4 @@ def rent():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all() # Creates the database file
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
